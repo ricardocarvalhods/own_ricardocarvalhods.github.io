ca12ec1071a97159d848aaa999677503190bfcb9
Ricardo Carvalho <ricardosc@gmail.com>
2019-12-26 01:43:37 -0300

    Create ricardo.js

---
+++ js/ricardo.js
@@ -0,0 +1,25 @@
+function contentFromFile(filename, elemToAppend){
+    // Create new div under body
+    var elem = document.createElement('div');
+    elem.setAttribute("id", filename);
+    elemToAppend.appendChild(elem);
+
+    // Get file and put on new div created
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4) {
+        if (this.status == 200) {
+          responseMD = this.responseText;
+          responseHTML = conv.makeHtml(responseMD)
+          document.getElementById(filename).innerHTML = responseHTML;
+          renderMathInElement(elem, {delimiters:[{left: "$", right: "$", display: false}, 
+                                                 {left: "$\\", right: "\\$", display: true}]});
+        }
+        if (this.status == 404) {
+          document.getElementById(filename).innerHTML = "Page not found.";
+        }
+      }
+    }
+    xhttp.open("GET", filename, true);
+    xhttp.send();
+}
