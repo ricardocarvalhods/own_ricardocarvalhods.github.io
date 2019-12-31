fd1ab8def6eb4911954379bdd2a291bcba9c56b4
Ricardo Carvalho <ricardosc@gmail.com>
2019-12-31 02:45:29 -0300

    Update ricardo.js

--- js/ricardo.js
+++ js/ricardo.js
@@ -87,8 +87,9 @@ function loadTitlesFromFolder(folder){
         else if(vl > 3){
             i_title = i_title + " " + name_split[vl]
         }
-        }      
-        counter_html = " &nbsp; &middot; &nbsp; <i class='fa fa-comments'></i> <span class='disqus-comment-count' data-disqus-identifier='ricardocarvalhods/html/page?" + folder + "/" + name_without_dot + "'> Comments</span>"
+        }
+        counter_html = ""
+        //counter_html = " &nbsp; &middot; &nbsp; <i class='fa fa-comments'></i> <span class='disqus-comment-count' data-disqus-identifier='ricardocarvalhods/html/page?" + folder + "/" + name_without_dot + "'> Comments</span>"
         final_mkdw = final_mkdw + "- **" + i_date + "**: [" + i_title + "](html/page?" + folder + "/" + name_without_dot +  ") " + counter_html  + " \n"
     }
     document.getElementById(folder).innerHTML = conv.makeHtml(final_mkdw);
